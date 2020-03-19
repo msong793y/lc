9787f1fe3c263982d95dd3bb7e90764ac620c2e9
msong793y <miles.song.ca@gmail.com>
2020-03-18 17:29:29 -0700

    solution works

--- lc463.js
+++ lc463.js
@@ -1,66 +1,59 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
 var islandPerimeter = function (grid) {
 
-    let dirs= [[-1,0],[1,0],[0,-1],[0,1]]
+    let dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
     let param = 0;
 
-    for(let i in grid){
-        for(let j in grid[i]){
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            param += helper([i, j], dirs)
 
-            param+= helper([i,j], dirs)
-
-        }    
+        }
     }
 
-    function helper(pos, dirs){
+    function helper(pos, dirs) {
 
-        if(grid[pos[0]][pos[1]]===0){return 0}
+        if (grid[pos[0]][pos[1]] === 0) { return 0 }
 
 
         let edge = 0;
-        
-        for (let i in dirs){
-            let dir =dirs[i]
-            console.log(grid[pos[0] + dir[0]][pos[1] + dir[1]])
-            let check = grid[pos[0]+dir[0]][pos[1]+dir[1]]
-            if (!check){
+
+        for (let i in dirs) {
+            let dir = dirs[i]
+
+            let ver = pos[0] + dir[0]
+
+            let hor = pos[1] + dir[1]
+           
+
+
+            if (ver < 0 || hor < 0 || ver >= grid.length || hor >= grid[pos[0]].length) {
                 edge++
+            } else {
+                let check = grid[ver][hor]
+                if (!check) {
+                    edge++
+                }
+
+
             }
+
+
         }
 
         return edge
 
-        // console.log(vert)
-        // console.log(hori)
-
-
-        // if(vert-1<0 || grid[vert-1][hori]===0){
-        //     edge++;
-        // }
-        // console.log(grid[vert][hori])
-
-        // if (vert+1 >= grid.length || grid[vert+1][hori] === 0) {
-        //     edge++;
-        // }
-        // if (hori - 1 < 0 || grid[vert][hori-1] === 0) {
-        //     edge++;
-        // }
-
-        // if (hori + 1 >= grid[vert].length || grid[vert][hori+1] === 0) {
-        //     edge++;
-        // }
-
-        return edge
 
     }
 
     return param
 
 
-};
+}
 
-let arr = [[0, 1, 0, 0],
-[1, 1, 1, 0],
-[0, 1, 0, 0],
-[1, 1, 0, 0]]
+let arr = [[0, 1]]
 
 console.log(islandPerimeter(arr))
